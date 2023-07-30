40c1b37f4ca86fa336dc808c5f533a668850596d
Maesro <maestrerodrigo@gmail.com>
2023-07-30 20:03:48 -0300

    update

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,55 +3,35 @@ import ItemList from '../ItemList/ItemList';
 import Spinner from '../Spinner/Spinner';
 import { useParams } from 'react-router-dom';
 //import { getProducts } from '../../data/asyncMock';
-import db from '../../services/firebase';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 
 const ItemListContainer = () => {
 
   const { categoryId } = useParams()
 
   const [items, setItems] = useState() //State donde grabo los items
-  const [load, setLoad] = useState(true) //Flag que me permite mostrar un spinner mientras cargo los datos
+  const [load, setLoad] = useState(true) //Flag que me permite mostrar un spinner mientras cargo los datos 
 
-  // useEffect(() => {
-  //   getProducts(categoryId)
-  //     .then(productos => {
-  //       setItems(productos)
-  //       setLoad(false)
-  //     })
-  //     .catch(err =>
-  //       console.warn(err)
-  //     )
-  // }, [categoryId])
+  // const getData = async (categoria) => {
+  //   setLoad(true)
+  //   const querydb = getFirestore();
+  //   const queryCollection = collection(querydb, 'Items');
+  //   const resultado = await getDocs(queryCollection)
+  //   const datos = resultado.docs.map(p => ({ id: p.id, ...p.data() }))
+  //   setItems(datos)
+  //   setLoad(false)
+  // } 
 
 
-  // const getDataCategory = async (categoria) => {
-  //   try {
-  //     setLoad(true)
-  //     const document = query(collection(db, "Items"), where("category", "==", categoria))
-  //     const col = await getDocs(document)
-  //     console.log("col.docs", col.docs)
-  //     const result = col.docs.map((doc) => doc = { id: doc.id, ...doc.data() })
-  //     setItems(result)
-  //     setLoad(false)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
   const getData = async (categoria) => {
-    try {
-      setLoad(true)
-      const document = categoria ? query(collection(db, "Items"), where("category", "==", categoria))
-        : collection(db, "Items")
-      const col = await getDocs(document)
-      const result = col.docs.map((doc) => doc = { id: doc.id, ...doc.data() })
-      setItems(result)
-      setLoad(false)
-    } catch (error) {
-      console.log(error)
-    }
+    setLoad(true)
+    const querydb = getFirestore();
+    const queryCollection = categoria ? query(collection(querydb, 'Items'), where("category", "==", categoria)) : collection(querydb, 'Items');
+    const resultado = await getDocs(queryCollection)
+    const datos = resultado.docs.map(p => ({ id: p.id, ...p.data() }))
+    setItems(datos)
+    setLoad(false)
   }
-
 
   useEffect(() => {
     getData(categoryId)
